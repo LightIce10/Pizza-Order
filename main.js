91f576ac855ca3ec661caba6c6622003f79cafbf
LightIce10 <145585156+LightIce10@users.noreply.github.com>
2023-11-20 09:43:57 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+// PIzza Order by Mr. V
+
+// Add an Event Listener to the button to run the placeOrder function
+document.getElementById("btn").addEventListener("click", placeOrder);
+
+//Store order code in a funcrion
+function placeOrder() {
+  // INPUT
+  let size = prompt("enter size of pizza (small, medium, large): ");
+  let topping1 = prompt("Enter first topping: ");
+  let topping2 = prompt("Enter second topping: ");
+
+  // PROCESS
+  let msg = `your ${size} pizza with ${topping1} and ${topping2} will be ready soon!`;
+
+  // OUTPUT
+  alert(msg);
+}
